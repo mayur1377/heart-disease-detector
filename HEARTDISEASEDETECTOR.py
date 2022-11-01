3e73db4befb23d0522458e6d7707e159a22f8ba3
MAYUR <74107137+mayur1377@users.noreply.github.com>
2022-11-01 09:23:26 +0530

    Update HEARTDISEASEDETECTOR.py

--- HEARTDISEASEDETECTOR.py
+++ HEARTDISEASEDETECTOR.py
@@ -4,12 +4,12 @@ import numpy as np
 import pandas as pd
 import pickle
 import sklearn
-import scipy
-import streamlit as st
-import pandas as pd
+# import scipy
+# import streamlit as st
+# import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.calibration import CalibratedClassifierCV
-from scipy.stats import zscore
+# from scipy.stats import zscore
 st.set_page_config(layout="wide")
 st.markdown("""
 <style>
